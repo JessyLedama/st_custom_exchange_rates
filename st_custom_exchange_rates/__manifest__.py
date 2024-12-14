a2c40f96fabb9d016230a950e42b3b099a2c6c88
Jayliste <jaylisteledama94@gmail.com>
2024-12-14 10:55:01 +0300

    [UPDATE] updated version to 17.0

--- st_custom_exchange_rates/__manifest__.py
+++ st_custom_exchange_rates/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Exchange Rate Management. Manage Exchange Rates in Sales, Customer Invoice, Vendor Bills, Journal Entries',
-    'version': '16.0.1.0.0',
+    'version': '17.0.1.0.0',
     'summary': 'Manage exchange rates from Sales, Invoices, Bills and journal entries. The resulting journal entries use this custom exchange rate.',
     'description': 'Manage exchange rates from Sales, Invoices, Bills and Journal Entries. The resulting journal entries use this custom exchange rate for debits and credits.',
     'author': 'SIMI Technologies',
