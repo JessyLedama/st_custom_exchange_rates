7917085216b383261ccc6a2847950e27431490bb
Jayliste <jaylisteledama94@gmail.com>
2024-12-14 09:37:32 +0300

    v1

---
+++ README.md
@@ -0,0 +1,3 @@
+## ODOO CUSTOM EXCHANGE RATE
+
+Manage exchange rates from Sales, Invoices, Bills and Journal Entries. The resulting journal entries use this custom exchange rate for debits and credits.
---
+++ st_custom_exchange_rate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Exchange Rate Management. Manage Exchange Rates in Sales, Customer Invoice, Vendor Bills, Journal Entries',
+    'version': '16.0.1.0.0',
+    'summary': 'Manage exchange rates from Sales, Invoices, Bills and journal entries. The resulting journal entries use this custom exchange rate.',
+    'description': 'Manage exchange rates from Sales, Invoices, Bills and Journal Entries. The resulting journal entries use this custom exchange rate for debits and credits.',
+    'author': 'SIMI Technologies',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+    'images': ['static/description/images/invil.png'],
+}
