1f4945f984524dc3c3b0d032041ae6eb4c956a41
Jayliste <jaylisteledama94@gmail.com>
2025-01-27 19:25:48 +0300

    [UPDATE] updated manifest

--- st_custom_exchange_rates/__manifest__.py
+++ st_custom_exchange_rates/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    'name': 'Exchange Rate Management. Manage Exchange Rates in Sales, Customer Invoice, Vendor Bills, Journal Entries, and Payments',
+    'name': 'Exchange Rate Management. Manage Exchange Rates in Sales, Purchase, Customer Invoice, Vendor Bills, Journal Entries, and Payments',
     'version': '17.0.1.0.0',
-    'summary': 'Manage exchange rates from Sales, Invoices, Bills, Journal Entries and Payments. The resulting journal entries use this custom exchange rate.',
-    'description': 'Manage exchange rates from Sales, Invoices, Bills, Journal Entries and Payments. The resulting journal entries use this custom exchange rate for debits and credits.',
+    'summary': 'Manage exchange rates from Sales, Purchase, Invoices, Bills, Journal Entries and Payments. The resulting journal entries use this custom exchange rate.',
+    'description': 'Manage exchange rates from Sales, Purchase, Invoices, Bills, Journal Entries and Payments. The resulting journal entries use this custom exchange rate for debits and credits.',
     'author': 'SIMI Technologies',
     'depends': ['account', 'sale', 'purchase'],
     'data': [
