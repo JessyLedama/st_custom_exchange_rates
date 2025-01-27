a5417856eff00e66036208d6cf82859a5e6747e0
Jayliste <jaylisteledama94@gmail.com>
2025-01-27 19:23:46 +0300

    [UPDATE] added to sale and purchase

--- st_custom_exchange_rates/__manifest__.py
+++ st_custom_exchange_rates/__manifest__.py
@@ -4,10 +4,12 @@
     'summary': 'Manage exchange rates from Sales, Invoices, Bills, Journal Entries and Payments. The resulting journal entries use this custom exchange rate.',
     'description': 'Manage exchange rates from Sales, Invoices, Bills, Journal Entries and Payments. The resulting journal entries use this custom exchange rate for debits and credits.',
     'author': 'SIMI Technologies',
-    'depends': ['account'],
+    'depends': ['account', 'sale', 'purchase'],
     'data': [
         'views/account_move_views.xml',
         'views/account_payment_views.xml',
+        'views/sale_order_views.xml',
+        'views/purchase_order_views.xml',
     ],
     'installable': True,
     'application': False,
